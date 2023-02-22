a393b50256b836f297ec6044b0d4cca1f099634b
vunguyenhuychuong <vunguyenhuychuong10t2@gmail.com>
2023-02-22 22:39:44 +0700

    upload2

--- BusinessObject/FStoreDBAssignmentContext.cs
+++ BusinessObject/FStoreDBAssignmentContext.cs
@@ -26,7 +26,7 @@ namespace BusinessObject
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=(local);uid=sa;pwd=9851;database=FStoreDBAssignment;TrustServerCertificate=True");
+                optionsBuilder.UseSqlServer("Server=(local);uid=sa;pwd=1010;database=FStoreDBAssignment;TrustServerCertificate=True");
             }
         }
 
